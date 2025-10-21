0944b5e4bbcc5180640d2e650189afd67bb27dbf
arayapudi626 <rayapudianisha@gmail.com>
2025-10-21 00:52:20 -0700

    update

--- lab23.cpp
+++ lab23.cpp
@@ -2,12 +2,15 @@
 #include <fstream>
 #include <iomanip>
 #include <list>
+#include <string>
+#include <cstdlib>
+#include <ctime>
 #include "Goat23.h"
 using namespace std;
 
 const int SZ_NAMES = 200, SZ_COLORS = 25, MAX_AGE = 20;
 
-int select_goat(list<Goat> trip);
+int select_goat(list<Goat> &trip);
 void delete_goat(list<Goat> &trip);
 void add_goat(list<Goat> &trip, string [], string []);
 void display_trip(list<Goat> trip);
@@ -15,7 +18,7 @@ int main_menu();
 
 int main() {
     srand(time(0));
-    bool again;
+    //bool again;
 
     // read & populate arrays for names and colors
     ifstream fin("names.txt");
@@ -35,8 +38,8 @@ while(true) {
     int choice = main_menu();
     switch (choice) {
         case 1: add_goat(trip, names, colors); break;
-        //case 2: delete_goat(trip); break;
-        //case 3: display_trip(trip); break;
+        case 2: delete_goat(trip); break;
+        case 3: display_trip(trip); break;
         case 4: return 0;
     }
 }
@@ -68,7 +71,7 @@ void add_goat(list<Goat> &trip, string names[], string colors[]){
     trip.push_back(new_goat);
     trip.sort();
 
-    cout << "Added goat: " << name << "( " << age << ", " << colors << " )\n";
+    cout << "Added goat: " << name << "( " << age << ", " << color << " )\n";
 }
 
 int select_goat(list<Goat> &trip){
@@ -88,9 +91,9 @@ int select_goat(list<Goat> &trip){
 void delete_goat(list<Goat> &trip){
     int count = 1;
     int choice = select_goat(trip);
-    for (auto it = trip.begin(); it != trip.end(), it++, count++){
+    for (auto it = trip.begin(); it != trip.end(); it++, count++){
         if (count == choice){
-            cout << "Deleting: " << it->get_name() << "\n";
+            cout << "Deleting: " << it->get_name() << " ( " << it->get_age() << ", " << it->get_color() << " ) \n";
             trip.erase(it);
             return;
         }
@@ -100,7 +103,11 @@ void delete_goat(list<Goat> &trip){
 }
 
 void display_trip(list<Goat> trip){
-
+    trip.sort();
+    int i = 1;
+    for (const Goat& g : trip){
+        cout << "[" << i++ << "] " << g.get_name() << " ( " << g.get_age() << ", " << g.get_color() << " ) \n";
+    }
 }
 
 
